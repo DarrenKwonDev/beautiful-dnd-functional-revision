a67c140154c336a0f7ffe0925db71d3ffdc0eeef
DarrenKwonDev <anatomy1545@gmail.com>
2020-12-14 02:26:21 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import initialData from "./initial-data";
+import Column from "./column";
+import { DragDropContext } from "react-beautiful-dnd";
 
 function App() {
+  const [state, setstate] = useState(initialData);
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    // dnd를 도중에 멈췄으므로(올바른 droppable 위에 두지 않았으므로) 그냥 리턴
+    if (!destination) {
+      return;
+    }
+
+    // 같은 자리에 가져다 두었다면 그냥 리턴
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setstate(newState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      {state.columnOrder.map((columnId) => {
+        const column = state.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
+    </DragDropContext>
   );
 }
 
